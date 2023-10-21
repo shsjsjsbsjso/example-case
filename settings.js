255ebfea7d5520ddcfcdf4d5ca99cebf8477de51
shsjsjsbsjso <148639262+shsjsjsbsjso@users.noreply.github.com>
2023-10-21 22:12:56 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -10,15 +10,15 @@ global.APIKeys = {
 	'https://api.zeeoneofc.my.id': 'Your Key',
 }
 
-global.namabot = "Bot Store"
-global.namaowner = "Zeeoneofc"
-global.packname = namabot
-global.author = namaowner
-global.myweb = "https://s.id/zeeoneofc"
-global.footer_text = "© " + namabot
+global.namabot = "XdimsBOT"
+global.namaowner = "XdimsDev"
+global.packname = XdimsBOT
+global.author = XdimsDev
+global.myweb = "wa.me/6281299609670"
+global.footer_text = "© " + XdimsBOT
 global.pp_bot = fs.readFileSync("./image/foto.jpg")
 global.qris = fs.readFileSync("./image/qris.jpg")
-global.owner = ['62887435047326']
+global.owner = ['xdimsDev']
 global.pairingNumber = "" //pairing number to install bot 
 global.sessionName = 'session'
 global.background = "https://telegra.ph/file/40342e0085d521cb8b907.jpg" //maks size 50kb agar welcome nya gk lemot
